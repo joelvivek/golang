6f3ed37c48939b8c3ac34dd64177ba948ce5fe05
joelvivek <joelvivek@Anitha-MacBook-Pro.local>
2019-06-22 11:57:13 +0530

    added string and int

--- functions/main.go
+++ functions/main.go
@@ -4,9 +4,16 @@ import "fmt"
 
 func main() {
 	card := newCard()
-	fmt.Println(card)
+	fmt.Println("card is", card)
+
+	cardnum := newCardNumber()
+	fmt.Println("cardnumber is", cardnum)
 }
 
 func newCard() string {
 	return "Five of Diamonds"
+}
+
+func newCardNumber() int {
+	return 12
 }
