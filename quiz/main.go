5ef156e00d604aa510bf8832afe9dc5cdecd7039
joelvivek <joelvivek@GladisJoels-MacBook-Pro.local>
2020-10-10 16:34:13 +0530

    quiz program

--- quiz/main.go
+++ quiz/main.go
@@ -1,1 +1,63 @@
-tests
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "csv file in the format of csv file")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("failed to open the file: %s \n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("failed to parse the provided csv file ")
+	}
+	fmt.Println(lines)
+	problems := parseLines(lines)
+	fmt.Println(problems)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			fmt.Println("correct")
+			correct++
+		}
+	}
+
+	fmt.Printf("you scored %d out of %d.\n", correct, len(problems))
+
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Printf(msg)
+	os.Exit(1)
+}
