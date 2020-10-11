9e5bbc472ce0fe5a6d9c7796bc6527d8ddc7715b
joelvivek <joelvivek@GladisJoels-MacBook-Pro.local>
2020-10-11 12:42:23 +0530

    add other program

--- simplerest/main.go
+++ simplerest/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "test title", Desc: " test description", Content: "content of article"},
+	}
+
+	fmt.Println("Endpoint hit : all article")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage endpoint hit")
@@ -12,7 +30,8 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8082", nil))
 }
 
 func main() {
